53e0d27da49243bf097a144c9ee4d8a673565e3e
Andrew Rice <acr31@cam.ac.uk>
2014-11-09 19:05:39 +0000

    Added helper class for building commands to send to docker

---
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/docker/CommandBuilder.java
@@ -0,0 +1,36 @@
+package uk.ac.cam.cl.dtg.teaching.docker;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.common.base.Joiner;
+
+public class CommandBuilder {
+
+	private List<String> commands;
+	
+	public CommandBuilder() {
+		commands = new LinkedList<String>();
+	}
+
+	public void add(String command) {
+		commands.add(command);
+	}
+	
+	public void addAll(Collection<String> commands) {
+		this.commands.addAll(commands);
+	}
+	
+	public void addAll(String[] commands) {
+		for(String s: commands) {
+			this.commands.add(s);
+		}
+	}
+	
+	public String[] toCmd() {
+		String toExec = Joiner.on(" && ").join(commands);
+		return new String[] { "/bin/bash", "-c", toExec };
+	}
+}
