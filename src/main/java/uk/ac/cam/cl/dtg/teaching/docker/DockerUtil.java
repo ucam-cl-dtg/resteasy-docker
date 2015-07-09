f3c9d010c7f706e073d1c904a7d24f8ee3b03781
Andrew Rice <acr31@cam.ac.uk>
2015-07-09 18:47:49 +0100

    added a couple of utility methods

--- src/main/java/uk/ac/cam/cl/dtg/teaching/docker/DockerUtil.java
+++ src/main/java/uk/ac/cam/cl/dtg/teaching/docker/DockerUtil.java
@@ -1,6 +1,11 @@
 package uk.ac.cam.cl.dtg.teaching.docker;
 
 import java.io.File;
+import java.util.List;
+
+import uk.ac.cam.cl.dtg.teaching.docker.api.DockerApi;
+import uk.ac.cam.cl.dtg.teaching.docker.model.Container;
+import uk.ac.cam.cl.dtg.teaching.docker.model.ContainerInfo;
 
 public class DockerUtil {
 
@@ -11,5 +16,28 @@ public class DockerUtil {
 	public static String bind(File host, File container, boolean readOnly) {
 		return host.getPath()+":"+container.getPath()+(readOnly?":ro":"");
 	}
-	
+
+	public static void deleteContainerByName(String name, DockerApi docker) {
+		List<Container> containers = docker.listContainers(true, null, null, null, null);
+		for(Container container : containers) {
+			String[] names = container.getNames();
+			if (names != null && names.length > 0 && names[0].equals("/"+name)) {
+				docker.deleteContainer(container.getId(), true, true);
+			}
+		}
+	}
+
+	public static boolean waitRunning(String containerID, DockerApi docker) {
+		for(int i=0;i<5;++i) {
+			ContainerInfo info = docker.inspectContainer(containerID);
+			if (info.getState().getPid() != 0) return true;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
 }
